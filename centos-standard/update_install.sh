594b3eddb258dbadd92565a7ba3df3434ce4ddd8
sa1dkcph <sa1dkcph@gmail.com>
2019-06-08 19:48:51 +0200

    committing all vagrantfile changes

--- centos-standard/update_install.sh
+++
@@ -1,4 +0,0 @@
-yum update -y
-yum install nano -y
-yum install openscap-scanner -y
-yum install scap-security-guide -y
--- ubuntu-standard/update_install.sh
+++ ubuntu-standard/update_install.sh
@@ -1,22 +1,22 @@
 apt-get update -y
-#apt-get install python -y
-#apt-get install libopenscap8 -y
-#apt-get install unzip -y
-#wget http://dk.archive.ubuntu.com/ubuntu/pool/universe/s/scap-security-guide/ssg-base_0.1.39-2_all.deb
-#wget http://dk.archive.ubuntu.com/ubuntu/pool/universe/s/scap-security-guide/ssg-debderived_0.1.39-2_all.deb
-#dpkg -i ssg-base_0.1.39-2_all.deb
-#dpkg -i ssg-debderived_0.1.39-2_all.deb
-#rm ssg-base_0.1.39-2_all.deb
-#rm ssg-debderived_0.1.39-2_all.deb
-#wget https://github.com/ComplianceAsCode/content/releases/download/v0.1.44/scap-security-guide-0.1.44-oval-510.zip
-#unzip scap-security-guide-0.1.44-oval-510.zip
-#rm scap-security-guide-0.1.44-oval-510.zip
-#mv /home/vagrant/scap-security-guide-0.1.44-oval-5.10 /home/vagrant/ssg
-#mv /usr/share/xml/scap/ssg/content /home/vagrant/
-#mv /home/vagrant/content /home/vagrant/ssg
-#cp /home/vagrant/ssg/ssg-ubuntu1604-cpe-dictionary.xml /usr/share/openscap/cpe
-#cp /home/vagrant/ssg/ssg-ubuntu1604-cpe-oval.xml /usr/share/openscap/cpe
-#mv /usr/share/openscap/cpe/ssg-ubuntu1604-cpe-dictionary.xml /usr/share/openscap/cpe/openscap-cpe-dict.xml
+apt-get install python -y
+apt-get install libopenscap8 -y
+apt-get install unzip -y
+wget http://dk.archive.ubuntu.com/ubuntu/pool/universe/s/scap-security-guide/ssg-base_0.1.39-2_all.deb
+wget http://dk.archive.ubuntu.com/ubuntu/pool/universe/s/scap-security-guide/ssg-debderived_0.1.39-2_all.deb
+dpkg -i ssg-base_0.1.39-2_all.deb
+dpkg -i ssg-debderived_0.1.39-2_all.deb
+rm ssg-base_0.1.39-2_all.deb
+rm ssg-debderived_0.1.39-2_all.deb
+wget https://github.com/ComplianceAsCode/content/releases/download/v0.1.44/scap-security-guide-0.1.44-oval-510.zip
+unzip scap-security-guide-0.1.44-oval-510.zip
+rm scap-security-guide-0.1.44-oval-510.zip
+mv /home/vagrant/scap-security-guide-0.1.44-oval-5.10 /home/vagrant/ssg
+mv /usr/share/xml/scap/ssg/content /home/vagrant/
+mv /home/vagrant/content /home/vagrant/ssg
+cp /home/vagrant/ssg/ssg-ubuntu1604-cpe-dictionary.xml /usr/share/openscap/cpe
+cp /home/vagrant/ssg/ssg-ubuntu1604-cpe-oval.xml /usr/share/openscap/cpe
+mv /usr/share/openscap/cpe/ssg-ubuntu1604-cpe-dictionary.xml /usr/share/openscap/cpe/openscap-cpe-dict.xml
 #wget https://people.canonical.com/~ubuntu-security/oval/com.ubuntu.xenial.cve.oval.xml
 
 #wget https://iasecontent.disa.mil/stigs/zip/U_Canonical_Ubuntu_16-04_LTS_V1R2_STIG.zip
